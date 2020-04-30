afeb245e22a44bbedbf093e3f3ddd4fc0a55cd55
Fabricio Altamirano <fabricio.altamirano@exaccta.com>
2020-04-30 16:36:33 +0200

    delete

--- AppTest/core/src/main/java/com/altamirano/fabricio/core/listeners/DialogResultListener.kt
+++
@@ -1,8 +0,0 @@
-package com.altamirano.fabricio.core.listeners
-
-import com.altamirano.fabricio.core.dialogs.DialogPassword
-
-interface DialogResultListener {
-    fun onCorrectPassword(dialog: DialogPassword, numberIntentsBefore:Int)
-    fun onErrorPassword(dialog: DialogPassword, numberIntents:Int)
-}
