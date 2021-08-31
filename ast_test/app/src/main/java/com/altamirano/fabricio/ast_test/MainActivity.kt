46a2b5deeb72fd51574d7c16bcdd03d091382690
Fabricio Altamirano <fabricio.altamirano@exaccta.com>
2021-08-31 12:57:52 +0200

    Update gradle

--- ast_test/app/src/main/java/com/altamirano/fabricio/ast_test/MainActivity.kt
+++ ast_test/app/src/main/java/com/altamirano/fabricio/ast_test/MainActivity.kt
@@ -2,10 +2,13 @@ package com.altamirano.fabricio.ast_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.altamirano.fabricio.core.dialogs.AstDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AstDialog(this).show()
     }
 }
